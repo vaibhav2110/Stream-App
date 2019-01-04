d34a4c6b804f2eb72eb82cb3ddf8bf3695a08d06
Vaibhav <vaibhpraky@gmail.com>
2019-01-04 12:31:51 +0530

    CRUD operations

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamCreate = ()=>{
-    return <div>StreamCreate</div>
+class StreamCreate extends React.Component{
+    onSubmit = (formValues)=>{
+        this.props.createStream(formValues);
+    }
+    render(){
+        return (
+            <div>
+                <h3>Create a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit}/>
+            </div>
+            );
+    }   
 }
 
-export default StreamCreate;
+export default connect(null, { createStream })(StreamCreate);
